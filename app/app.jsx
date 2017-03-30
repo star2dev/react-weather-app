cbec7018ca7a773139ab22e05d23c4f55875c94b
Bruce Galpin <bruce.galpin@arqino.com>
2017-03-30 19:46:43 +0200

    First commit

---
+++ app/app.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Examples = require('Examples');
+
+// http://samples.openweathermap.org/data/2.5/find?q=London&units=metric&appid=829aacc7cd5aa4bf8171d9f4be47bf8a
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
+      <Route path="examples" component={Examples}/>
+      <IndexRoute component={Weather}/>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
---
+++ app/components/Nav.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h2>Nav Component</h2>
+        <IndexLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/">Get Weather</IndexLink>
+        <Link activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="about">About</Link>
+        <Link activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="examples">Examples</Link>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
