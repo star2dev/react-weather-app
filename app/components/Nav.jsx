226e8d7b4d29aea198881546d36b2bc0529668e7
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-02 10:21:22 +0200

    Fixed search function

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,11 +5,11 @@ var Nav = React.createClass({
   onSearch: function (e) {
     e.preventDefault();
 
-    var location = this.refs.location.value;
+    var location = this.refs.search.value;
     var locEncode = encodeURIComponent(location);
 
     if (location.length > 0) {
-      this.refs.location.value = '';
+      this.refs.search.value = '';
       window.location.hash = '#/?location=' + locEncode;
     }
   },
@@ -34,7 +34,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" ref="location" placeholder="Search weather by city"/>
+                <input type="search" placeholder="Search weather by city" ref="search"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
