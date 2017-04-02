8043c3b18b21225892d4b02eb1e5b0abda5cfb1d
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-02 10:01:06 +0200

    Wired nav search function

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function (e) {
     e.preventDefault();
-    alert('Not yet wired');
+
+    var location = this.refs.location.value;
+    var locEncode = encodeURIComponent(location);
+
+    if (location.length > 0) {
+      this.refs.location.value = '';
+      window.location.hash = '#/?location=' + locEncode;
+    }
   },
   render: function () {
     return (
@@ -27,7 +34,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city"/>
+                <input type="search" ref="location" placeholder="Search weather by city"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
